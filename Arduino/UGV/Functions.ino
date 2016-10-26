798e63fb46f57187fc4e531f88d2d2f833a28799
jlrandulfe <jlrandulfe@gmail.com>
2016-10-26 13:19:34 +0200

    Added comments and limitted values to function move_robot

--- Arduino/UGV/Functions.ino
+++ Arduino/UGV/Functions.ino
@@ -16,7 +16,69 @@ void process_message(char raw_data[]){
       sending_function_code = ACK_MSG;
   }
   publish_data(sending_function_code, 0,output_data);
-}
+}    
+
+
+/*
+ * Function: move_robot
+ * --------------------
+ * Right and left wheels speed control.
+ * Values from 0 to 127 are interpreted as reverse direction.
+ * Values from 128 to 255 are interpreted as direct direction.
+ *
+ * Once the direction is checked, the output has to be rescaled 
+ * to [0..255], as it is the full range of the output.
+*/
+void move_robot(unsigned char a,unsigned char b)
+{
+  boolean right_direction;
+  boolean left_direction;
+  // Reverse direction
+  if (a<128){
+    if (a<0){
+      a = 0;
+    }
+    a *= 2;
+    a++;
+    a = 255 - a;
+    right_direction = true;
+  }
+  // Direct direction
+  else{
+    if (a>255){
+      a = 255;
+    }    
+    a -= 128;
+    a *= 2;
+    a++;
+    right_direction = false;
+    
+  }
+  if (b<128){
+    if (b<0){
+      b = 0;
+    }      
+    b *= 2;
+    b++;
+    b = 255 - b;
+    left_direction = true;
+  }    
+  else{
+    if (b>255){
+      b = 255;
+    }    
+    b -= 128;
+    b *= 2;
+    b++;
+    left_direction = false;    
+  }
+  // Send the values to the corresponding pins.
+  analogWrite (PIN_PWM_R,a);      
+  digitalWrite(PIN_MOT_R, right_direction);    
+  analogWrite (PIN_PWM_L,b);    
+  digitalWrite(PIN_MOT_L,  left_direction);
+}  
+
 
 
 
@@ -37,51 +99,6 @@ void publish_data(char fun_code, unsigned long int len, char* data) {
   }   
   Serial.print(etx);  
 }    
-     
-     
-      
-
-// Right and left wheels speed control
-void move_robot(unsigned char a,unsigned char b)
-{
-  boolean right_direction;
-  boolean left_direction;
-  
-//  int i = a;
-  if (a<128){
-    a *= 2;
-    a++;
-    a = 255 - a;
-    right_direction = true;
-  }
-  else{
-    a -= 128;
-    a *= 2;
-    a++;
-    right_direction = false;
-    
-  }
-  if (b<128){
-    b *= 2;
-    b++;
-    b = 255 - b;
-    left_direction = true;
-  }    
-  else{
-    b -= 128;
-    b *= 2;
-    b++;
-    left_direction = false;
-    
-  }
-  analogWrite (PIN_PWM_R,a);      
-  digitalWrite(PIN_MOT_R, right_direction);    
-  analogWrite (PIN_PWM_L,b);    
-  digitalWrite(PIN_MOT_L,  left_direction);
-}  
-
-
-
 
       
     
