6aef01c6f60404d704cdd6597d9622c67c6ba660
jlrandulfe <jlrandulfe@gmail.com>
2016-11-03 16:24:55 +0100

    Added time measurements

--- uvispace/uvirobot/messenger.py
+++ uvispace/uvirobot/messenger.py
@@ -17,6 +17,7 @@ import glob
 import struct
 import sys
 import getopt
+import time
 # ROS libraries
 import rospy
 from geometry_msgs.msg import Twist
@@ -64,18 +65,40 @@ def messenger_shutdown():
     stop_speed.angular.z = 0.0
     move_robot(stop_speed, my_serial)
     
+    wait_mean_time = sum(wait_times) / len(wait_times)
+    speed_calc_mean_time = sum(speed_calc_times) / len(speed_calc_times)
+    xbee_mean_time = sum(xbee_times) / len(xbee_times)
+    print ('Wait mean time: {wait}\n'
+           'Speed calculation mean time: {speed}\n'
+           'XBee message sending mean time: {xbee}'
+           .format(wait=wait_mean_time, speed=speed_calc_mean_time,
+                   xbee=xbee_mean_time)
+          )
+    
 def move_robot(data, serial):
     """Converts Twist msg into 2WD value and send it through port."""
     #Change proposal. In order to accept all the parameterfs
 #    serial = args[0]
 #    robot_speed = args[1]
+    global t0
+    global t1
+    global t2
+    t1 = time.time()
+    wait_times.append(t1-t0)
+    rospy.loginfo('New set point received')
     linear = data.linear.x
     angular = data.angular.z
+    t2 = time.time()
+    speed_calc_times.append(t2-t1)
     robot_speed.set_speed([linear, angular], 'linear_angular')
     robot_speed.get_2WD_speeds()
-    v_RightWheel, v_LeftWheel = robot_speed.nonlinear_transform()
+    v_RightWheel, v_LeftWheel = robot_speed.nonlinear_transform(min_A=70,
+                                                                max_B=190)
     rospy.loginfo('I am sending R: {} L: {}'.format(v_RightWheel, v_LeftWheel))
     serial.move([v_RightWheel, v_LeftWheel])
+    t0 = time.time()
+    xbee_times.append(t0-t2)
+    rospy.loginfo('Transmission ended succesfully\n\n')
 
 
 if __name__ == "__main__":
@@ -95,9 +118,14 @@ if __name__ == "__main__":
             sys.exit()
         elif opt in ("-r", "--robotid"):
             robot_id = int(arg)
+    t0 = time.time()
+    wait_times = []
+    speed_calc_times = []
+    xbee_times = []
     #Creates an instance of SerMesProtocol and checks connection to port
     my_serial = connect_and_check(robot_id)
     robot_speed = Speed()
+    t0 = time.time()
     listener(robot_id, robot_speed, my_serial)
     rospy.on_shutdown(messenger_shutdown)    
     #Keeps python from exiting until this node is stopped
