b43493a74b42dc92823e297b1a7ae2a63ada691c
jlrandulfe <jlrandulfe@gmail.com>
2016-11-21 12:36:07 +0100

    Improved comments and corrected minor bugs

--- uvispace/uvisensor/client.py
+++ uvispace/uvisensor/client.py
@@ -5,7 +5,7 @@ This module contains the class Client, which inherits from socket.socket
 * socket.socket class source code can be found in the folowing link:
 https://hg.python.org/cpython/file/2.7/Lib/socket.py
 
-* The socket.socket class is a subclass of the _socket.socket class.
+* This class is a child of the _socket.socket class.
 _socket.socket class source code can be found in the following link:
 https://github.com/biosbits/bits/blob/master/python/_socket.py
 """
@@ -15,22 +15,23 @@ import socket
 from socket import socket as Socket
 
 class Client(Socket):
-    """
-    A child class of socket.socket which includes register operations.
+    """Child class of socket.socket which includes register operations.
     
     It is intended to work with registers of a device, namely of an 
     FPGA. The register identifiers must correspond with the ones of the
-    designed circuit.
+    designed HDL circuit.
     
-    Methods from parent class:
+    Used methods from parent class:
     
     * connect((host, port)) : connects to a remote address
+    * close()
     * send(data) 
     * recv(buflen) : read the specified number of bytes
+    * settimeout(timeout)
     """
-    def __init__(self, ip, port, buffer_size=2048, timeout=5.0):
-        """Creates the socket object.
-        
+    def __init__(self, ip, port, buffer_size=2048, timeout=1.0):
+        """Set attributes, inheritance and logger.
+                
         Parameters
         ----------
         ip : string
@@ -49,6 +50,7 @@ class Client(Socket):
         """
         #Initializes parent class
         Socket.__init__(self, family=socket.AF_INET, type=socket.SOCK_STREAM)
+        #Connection parameters for the client device
         self.ip = ip
         self.port = port
         self.buffer_size = buffer_size
@@ -57,10 +59,9 @@ class Client(Socket):
         self.settimeout(timeout)
 
     def open_connection(self):
-        """Creates the socket connection.
+        """Create the socket connection.
         
-        It uses the class ip and port attributes as the connection 
-        address. It writes to log the connection.
+        After connecting, the input buffer is read for emptying it.
         """
         self.connect((self.ip, self.port))
         self._logger.info('Started TCP client with IP: {} '
@@ -69,43 +70,55 @@ class Client(Socket):
         self.recv(self.buffer_size)
         
     def close_connection(self):
-        """
+        """Send close command to device and close connection.
+        
         Write 'Q' to FPGA register and call Socket.close() method.
         """
-        #The parent class changes _sock class when it is closed.
+        #Check parent class _sock variable,
+        #as it changes its class when it is closed.
+        #WARNING: not valid method for Python3.
         if not isinstance(self._sock, socket._closedsocket):
             self.send('Q\n')
             self.close()
-        self._logger.info('Closed the TCP client')
+            self._logger.info('Closed the TCP client')
+        else:
+            self._logger.debug('Unable to close TCP client. Already closed')
         
     def read_data(self, size):
-        """Reads a size number of data bytes from the socket buffer."""
+        """Read N packages and return a cocatenation of all of them.
+        
+        Parameters
+        ----------
+        size : int
+            Number of bytes wished to be read.
+        """
         bytes = 0
         packages = []
+        #Do not stop reading new packages until target 'size' is reached.
         while (bytes < size):
-            data = self.sock.recv(self.buffer_size)
-            bytes += len(data)
-            packages.append(data)
+            received_package = self.sock.recv(self.buffer_size)
+            bytes += len(received_package)
+            packages.append(received_package)
             _logger.debug('Received {} bytes of {} ({:.2f})\r'
                           ''.format(bytes, size, (100 * float(bytes)/size))
+                         )
+        #Cocatenate all the packages in a unique variable
         data = ''.join(packages)
         return data
         
     def write_command(self, data):
-        """
-        Writes a command value in the  buffer and sends it to TCP client.
-        """
+        """Send a command to the TCP client and return the response."""
         self.send('{}\n'.format(data))
-        return self.read()
+        return self.recv(self.buffer_size)
     
     def read_register(self, reg):
-        """Reads the value from a register ."""
+        """Read the value of a register."""
         self.send('r,{}\n'.format(reg))
         result = self.recv(self.buffer_size)
         return eval(result)
 
     def write_register(self, reg, value):
-        """Writes a value into a register."""
+        """Write a value into a register and clean up input buffer."""
         self.send('w,{},{}\n'.format(reg, value))
         return self.recv(self.buffer_size)
 
