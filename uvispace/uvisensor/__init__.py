00a77c8e3ba51d7d37953c570da44a0393ce53a6
jlrandulfe <jlrandulfe@gmail.com>
2017-01-31 18:03:35 +0100

    Created a missing __init__.py file inside the uvisensor package. It is necessary for using the folder as a Python package.

---
+++ uvispace/uvisensor/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""uvirobot package from UviSpace project
+
+This python package has 2 main modules:
+
+* robot: hears to published poses and goals and calculates new paths.
+* messenger: receives speed set points and send them to the UGV through
+serial ports.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+__all__ = ['client', 'geometry', 'imgprocessing', 'multiplecamera', 'videosensor']
+
