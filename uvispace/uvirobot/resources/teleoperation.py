8f4774c0a4d38232a0a18f3666e19a9de804af62
joslamasvarela <joslamasvarela@gmail.com>
2017-04-27 10:14:00 +0200

    Removed 'Enter' requirement and added left and right movements

--- uvispace/uvirobot/resources/teleoperation.py
+++ uvispace/uvirobot/resources/teleoperation.py
@@ -11,16 +11,24 @@ import rospy
 Auxiliary program for controlling the UGV movements through keyboard.
 """
 
-def getKey(settings):
-    #Wait until stdin is ready to be read. There is a timeout of 0.1s
-    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+def getKey():
+    """Return key pressed."""
+    #file descriptor (integer that represents an open file).
+    fd = sys.stdin.fileno()
+    #get stdin settings
+    settings = termios.tcgetattr(fd)
+    #Put terminal in raw mode
+    tty.setraw(fd)
+    #Wait until stdin is ready to be read. There is a timeout of 0.25s.
+    #This time depends on the keyboard delay in the operating system.
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.25)
     #Read the stdin input.
     if rlist:
         key = sys.stdin.read(1)
     else:
         key = ''
-    #Set the attributes stored in settings after transmitting queued output
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    #Set the attributes stored in settings after transmitting queued output.
+    termios.tcsetattr(fd, termios.TCSADRAIN, settings)
     return key
 
 if __name__=="__main__":
@@ -28,42 +36,65 @@ if __name__=="__main__":
     rospy.init_node('teleop_keyboard')
     pub_vel = rospy.Publisher('/robot_1/cmd_vel', Twist, queue_size=1)
     speeds = Twist()
-    #get stdin settings
-    settings = termios.tcgetattr(sys.stdin)
-    #Put terminal in raw mode
-    tty.setraw(sys.stdin.fileno())
     init_time = time.time()
-    print ('Teleoperation program initialized. Available commands:\n\r'
+    #instructions for moving the UGV.
+    print ('\n\r'
+           'Teleoperation program initialized. Available commands:\n\r'
            '* S : Move backwards.\n\r'
-           '* W : Move backwards.\n\r'
-           '* Z : Stop moving.\n\r'
+           '* W : Move forward.\n\r'
+           '* A : Move left. \n\r'
+           '* D : Move right. \n\r'
            '* Q : Stop and quit.\n\r'
+           '* Nothing or another key : Stop moving.\n\r'
+           '\n\r'
+           'Currently stop moving \n\r'
           )
-#    print ('\n\r'.join(message))
+    #this initialization is necessary to update the previous state variable
+    #key (prev_key) the first time.
+    key = ''
+    #print ('\n\r'.join(message))
     while True:
-        key = getKey(settings)
+        #variables key pressed now and key previously pressed
+        prev_key = key
+        key = getKey()
         #Move forward.
         if key in ('w','W'):
-            print 'Moving forward'
+            message = 'moving forward'
             speeds.linear.x = 190
+            speeds.angular.z = 0
             pub_vel.publish(speeds)
         #Move backwards.
-        if key in ('s','S'):
-            print 'Moving backwards'
+        elif key in ('s','S'):
+            message = 'moving backwards'
             speeds.linear.x = -300
+            speeds.angular.z = 0
             pub_vel.publish(speeds)
-        #Stop moving.
-        if key in ('z','Z'):
-            print 'Stopping moving'
-            speeds.linear.x = 0
+        #Move left.
+        elif key in ('a','A'):
+            message = 'moving left'
+            speeds.angular.x = 0
+            speeds.angular.z = 10
+            pub_vel.publish(speeds)
+        #Move right.
+        elif key in ('d','D'):
+            message = 'moving right'
+            speeds.angular.x = 0
+            speeds.angular.z = -10
             pub_vel.publish(speeds)
         #Stop moving and exit.
-        if key in ('q','Q'):
-            print 'Stopping and exiting program'
+        elif key in ('q','Q'):
+            print ('Stop and exiting program. Have a good day! =)')
             speeds.linear.x = 0
+            speeds.angular.z = 0
             pub_vel.publish(speeds)
             break
-
-
-
-
+        #Stop moving.
+        else:
+            message = 'stop moving'
+            speeds.linear.x = 0
+            speeds.angular.z = 0
+            pub_vel.publish(speeds)
+        #if key pressed now and key pressed previously are different, update
+        #message
+        if prev_key != key:
+            print 'Currently %s. \n\r' % message
