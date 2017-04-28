69d1be9a5dab7623424d5268976147a3db34abdf
floonone <floonone@users.noreply.github.com>
2017-04-28 17:39:43 +0200

    Remove ros dependency from robot.py
    Replaced ros publisher with zmq publisher socket.
    Changed format of speed data from Twist to dictionary.

--- uvispace/uvirobot/robot.py
+++ uvispace/uvirobot/robot.py
@@ -2,14 +2,15 @@
 """This module communicates with user and sensors for finding paths.
 
 It contains a class, *RobotController*, that represents a real UGV, and 
-contains ROS functionality for publishing new speed values, UGV's 
+contains ROS functionality for publishing new speed values, UGVs 
 attributes, such as the *robot_id*, its speed values, or an instance 
 of the *PathTracker*, for calculating and storing the robot navigation 
 values.
 """
-# ROS libraries
-import rospy
-from geometry_msgs.msg import Twist
+# Standard libraries
+import logging
+import zmq
+
 # Local libraries
 import path_tracker
 
@@ -25,35 +26,41 @@ class RobotController(object):
         """Class constructor method"""
         self.robot_id = robot_id
         self.init = False
-        self.speeds = Twist()
+        self.speeds = {
+            'linear': 0.0,
+            'angular': 0.0
+        }
         self.QCTracker = path_tracker.QuadCurveTracker()
-        self.pub_vel = rospy.Publisher('/robot_{}/cmd_vel'.format(robot_id),
-                                       Twist, queue_size=1)
+
+        pub_vel = zmq.Context.instance().socket(zmq.PUB)
+        # FIXME [floonone-20170428] hardcoded socket bind
+        pub_vel.bind("tcp://*:35011")
+        self.pub_vel = pub_vel
 
     def set_speed(self, pose):
         """
         Receives a new pose and calculates the UGV speeds.
 
-        After calculating the new speed value, it is published on the 
-        rostopic *'/robot_X/cmd_vel'* using the *pub_vel* object. 
+        After calculating the new speed value, the dictionary containing 
+        the new speed values is published via the pub_vel socket.
         Only the values X, Y and theta of the *Pose2D* type are used, as 
         the designed Space consists in a 2-D flat space.
 
         :param pose: contains a 2-D position, with 2 cartesian values 
          (x,y) and an angle value (theta).
-        :type pose: gemoetry_msgs.Pose2D
+        :type pose: geometry_msgs.Pose2D
         """
-        if self.init == False:
+        if not self.init:
             self.QCTracker.append_point((pose.x, pose.y))
             self.init = True
         linear, angular = self.QCTracker.run(pose.x, pose.y, pose.theta)
-        rospy.loginfo('\nLocation--> '
-                      'X: {pose.x}, Y: {pose.y}, theta: {pose.theta} \n'
-                      'Speeds--> Linear: {linear}, Angular {angular}'.format(
+        logging.info('\nLocation--> '
+                     'X: {pose.x}, Y: {pose.y}, theta: {pose.theta} \n'
+                     'Speeds--> Linear: {linear}, Angular {angular}'.format(
                             pose=pose, linear=linear, angular=angular))
-        self.speeds.linear.x = linear
-        self.speeds.angular.z = angular
-        self.pub_vel.publish(self.speeds)
+        self.speeds['linear'] = linear
+        self.speeds['angular'] = angular
+        self.pub_vel.send_json(self.speeds)
 
     def new_goal(self, goal):
         """
@@ -68,13 +75,15 @@ class RobotController(object):
             # Adds the new goal to the current path, calculating all the 
             # intermediate points and stacking them to the path array
             self.QCTracker.append_point(goal_point)
-            rospy.loginfo('New goal--> X: {}, Y: {}'.format(goal.x, goal.y))
+            logging.info('New goal--> X: {}, Y: {}'.format(goal.x, goal.y))
         else:
-            rospy.loginfo(' The system is not yet initialized. \
-            Waiting for a pose to be published ')
+            logging.info(' The system is not yet initialized. '
+                         'Waiting for a pose to be published ')
 
     def on_shutdown(self):
-        """ Shutdown method. Is called when execution is aborted."""
-        self.speeds.linear.x = 0.0
-        self.speeds.angular.z = 0.0
-        self.pub_vel.publish(self.speeds)
+        """
+        Shutdown method. Is called when execution is aborted.
+        """
+        self.speeds['linear'] = 0.0
+        self.speeds['angular'] = 0.0
+        self.pub_vel.send_json(self.speeds)
