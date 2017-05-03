68476e0ce03e7e6b42f911b26b6aa24d5afc83ef
floonone <floonone@users.noreply.github.com>
2017-05-03 12:44:26 +0200

    Remove ros dependency from sim_sensors.py
    Replaced ros publisher with zmq publisher socket.
    Changed format of position data from Pose2D to dictionary.

--- uvispace/uvisensor/resources/sim_sensors.py
+++ uvispace/uvisensor/resources/sim_sensors.py
@@ -3,17 +3,28 @@
 This module simulates the uvisensor package for testing uvirobot.
 
 A robot is simulated to be placed at the coordinate (1,1) with an angle
-of 0 radians. The pose is published to the topic every 25 milliseconds.
+of 0 radians. The pose is published every 25 milliseconds.
 """
-import rospy
-from geometry_msgs.msg import Pose2D
 import time
+import zmq
 
-rospy.init_node('ispace_sensor')
-pub_pose_1 = rospy.Publisher('/robot_1/pose2d', Pose2D, queue_size=1)
-x = 1.0
-y = 1.0
-theta = 0.0
-while not rospy.is_shutdown():
-    pub_pose_1.publish(Pose2D(x, y, theta))
-    time.sleep(0.025)
+
+def main():
+    publisher = zmq.Context.instance().socket(zmq.PUB)
+    # FIXME [floonone-20170503] hardcoded socket bind
+    publisher.bind("tcp://*:35001")
+    position = {
+        'x': 1.0,
+        'y': 1.0,
+        'theta': 0.0
+    }
+    try:
+        while True:
+            publisher.send_json(position)
+            time.sleep(0.025)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
