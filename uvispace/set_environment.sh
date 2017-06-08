50c8f39db216f95584e47d522a069006d825b680
jlrandulfe <jlrandulfe@gmail.com>
2017-06-08 10:37:53 +0200

    Check if environment script was correctly sourced
    Add as well an output message that will report if the script was
    correctly sourced or not.

--- uvispace/set_environment.sh
+++ uvispace/set_environment.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# This statement will raise an error if the file was run, instead of sourced.
+# It also redirects the STDOUT and STDERR to /dev/null.
+# The null device is a device file that discards all data written to it but
+# reports that the write operation succeeded.
+$(return >/dev/null 2>&1)
+# Print error message.
+if ! [ "$?" -eq "0" ];
+then
+	echo "-------------------------WARNING-------------------------"
+    echo "This script has not been sourced."
+    echo "Please type on your terminal: 'source set_environment.sh'"
+    exit 1
+fi
+
 export OLDPYTHONPATH=$PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$parent_path
 
@@ -9,3 +23,5 @@ export UVISPACE_LOG_PATH=$parent_path/log
 export UVISPACE_BASE_PORT_POSITION=35000
 export UVISPACE_BASE_PORT_SPEED=35010
 export UVISPACE_BASE_PORT_GOAL=35020
+
+echo "Exported the environment variables for the uvispace project."
