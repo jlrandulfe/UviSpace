f6556b2409ad64b4100bf47c736bf5c3fd663981
jlrandulfe <jlrandulfe@gmail.com>
2017-06-12 09:54:46 +0200

    Remove hps2fpga reset and synchronize the soft reset

--- DE1-SoC/FPGA_Design/uvispace_top.v
+++ DE1-SoC/FPGA_Design/uvispace_top.v
@@ -206,7 +206,7 @@ integer vga_col;
 wire	  [11:0] CCD_DATA;
 //CCD_Capture signals
 wire    [11:0] ccd_data_captured;		//output data from CCD_Capture
-wire				ccd_dval;            //valid output data
+wire				   ccd_dval;            //valid output data
 wire    [15:0] X_Cont;
 wire	  [15:0] Y_Cont;
 reg     [11:0] ccd_data_raw;		    //input raw data to CCD_Capture
@@ -366,7 +366,7 @@ camera_capture u3(
   .in_start     (SW[9]),
   .clock        (ccd_pixel_clk),
   // Negative logic reset
-  .reset_n      (hps2fpga_reset_n & video_stream_reset_n),
+  .reset_n      (video_stream_reset_n),
   .in_width     (in_width[11:0]),
   .in_height    (in_height[11:0])
   );
@@ -397,9 +397,9 @@ camera_capture u3(
   assign  CCD_LVAL     =  GPIO_1[21]; //line valid
   assign  ccd_pixel_clk=  GPIO_1[0];  //Pixel clock
   assign  GPIO_1[19]   =  1'b1;       //trigger
-  assign  GPIO_1[17]   =  hps2fpga_reset_n & video_stream_reset_n;
+  assign  GPIO_1[17]   =  video_stream_reset_n;
   
-  assign  GPIO_0[0]   =  hps2fpga_reset_n & video_stream_reset_n;
+  assign  GPIO_0[0]   =  video_stream_reset_n;
   assign  GPIO_0[1]   =  ccd_pixel_clk;
 
   // Refreshes the data on the CCD camera on every pixel clock pulse.
@@ -421,7 +421,7 @@ corresponding pixel afterwards.
 raw2rgb u4(	
   .iCLK         (ccd_pixel_clk),
   // Negative logic reset
-  .iRST         (hps2fpga_reset_n & video_stream_reset_n),
+  .iRST         (video_stream_reset_n),
   .iDATA        (ccd_data_captured),  // Component input data
   .iDVAL        (ccd_dval),           // Data valid signal
   .oRed         (raw_rgb_red),        // Output red component
@@ -438,7 +438,7 @@ raw2rgb u4(
   // In case that only one FIFO memory is used, only the 5 most significative 
   // bits of each component are sent to the SDRAM.
   always @(posedge ccd_pixel_clk) begin
-    if (!hps2fpga_reset_n & video_stream_reset_n) begin
+    if (!video_stream_reset_n) begin
       // if reset, do nothing.
     end
     else begin
@@ -456,7 +456,7 @@ raw2rgb u4(
 
 rgb2hue hue(
   .clock(ccd_pixel_clk),
-  .reset_n(hps2fpga_reset_n & video_stream_reset_n),
+  .reset_n(video_stream_reset_n),
   // Data input
   .in_red(raw_rgb_red[11:4]),
   .in_green(raw_rgb_green[11:4]),
@@ -484,7 +484,7 @@ rgb2hue hue(
 image_capture imgcap1 (
 	// Clock and reset
 	.clk ( ccd_pixel_clk ),
-	.reset_n (hps2fpga_reset_n & video_stream_reset_n),
+	.reset_n (video_stream_reset_n),
 	// Signals from the video stream
 	.R( hue_red ),
 	.G( hue_green ),
@@ -538,7 +538,7 @@ Sdram_Control u1(
   .WR1_ADDR(0),
   .WR1_MAX_ADDR(640*480),             //address bus size: 25 bits
   .WR1_LENGTH(9'h80),                 //Max allowed size: 8 bits
-  .WR1_LOAD(!(hps2fpga_reset_n & video_stream_reset_n)),
+  .WR1_LOAD(!video_stream_reset_n),
   .WR1_CLK(~ccd_pixel_clk),
   // FIFO Write Side 2 (Unused. Needed if 8 bits per pixel are used)
   .WR2_DATA(fifo1_writedata),         //data bus size: 16 bits
@@ -546,7 +546,7 @@ Sdram_Control u1(
   .WR2_ADDR(22'h100000),
   .WR2_MAX_ADDR(22'h100000+640*480),  //address bus size: 25 bits
   .WR2_LENGTH(9'h80),                 //Max allowed size: 8 bits
-  .WR2_LOAD(!(hps2fpga_reset_n & video_stream_reset_n)),
+  .WR2_LOAD(!video_stream_reset_n),
   .WR2_CLK(~ccd_pixel_clk),
   // FIFO Read Side 1
   .RD1_DATA(fifo1_readdata),          //data bus size: 16 bits
@@ -554,7 +554,7 @@ Sdram_Control u1(
   .RD1_ADDR(0),     
   .RD1_MAX_ADDR(640*480),             //address bus size: 25 bits
   .RD1_LENGTH(9'h80),                 //Max allowed size: 8 bits
-  .RD1_LOAD(!(hps2fpga_reset_n & video_stream_reset_n)),
+  .RD1_LOAD(!video_stream_reset_n),
   .RD1_CLK(~clk_25),
   // FIFO Read Side 2 (Unused. Needed if 8 bits per pixel are used)
   .RD2_DATA(fifo2_readdata),          //data bus size: 16 bits
@@ -562,7 +562,7 @@ Sdram_Control u1(
   .RD2_ADDR(22'h100000),     
   .RD2_MAX_ADDR(22'h100000+640*480),  //address bus size: 25 bits
   .RD2_LENGTH(9'h80),                 //Max allowed size: 8 bits
-  .RD2_LOAD(!(hps2fpga_reset_n & video_stream_reset_n)),
+  .RD2_LOAD(!video_stream_reset_n),
   .RD2_CLK(~clk_25),
   // SDRAM Side
   .SA(DRAM_ADDR),
@@ -582,7 +582,7 @@ Sdram_Control u1(
 // VGA controller component.
 vga_controller vga_component(
   .pixel_clk  ( clk_25 ),
-  .reset_n    ( hps2fpga_reset_n & video_stream_reset_n ),
+  .reset_n    ( video_stream_reset_n ),
   .h_sync     ( VGA_HS ),
   .v_sync     ( VGA_VS ),
   .disp_ena   ( vga_enable ),
@@ -663,7 +663,7 @@ camera_config #(
   ) camera_conf(
   // Host Side
   .clock(ccd_pixel_clk),
-  .reset_n(hps2fpga_reset_n & video_stream_reset_n),
+  .reset_n(video_stream_reset_n),
   // Configuration registers
   .exposure(in_exposure),
   .start_row(in_start_row),
@@ -696,7 +696,19 @@ camera_config #(
   // assign in_row_mode = 16'h0011;
   // assign in_column_mode = 16'h0011;
   
+// Synchronization of the software reset with the pixel clock.
+reg sync_soft_reset;
+reg _camera_soft_reset_n;
+always @(posedge ccd_pixel_clk) begin
+  _camera_soft_reset_n = camera_soft_reset_n;
+  if (_camera_soft_reset_n != camera_soft_reset_n) begin
+    sync_soft_reset = 0;
+  end
+  else begin
+    sync_soft_reset = 1;
+  end
+end
 // Reset logic
-assign video_stream_reset_n = (camera_soft_reset_n & KEY[0]);
+assign video_stream_reset_n = (sync_soft_reset & KEY[0]);
 
 endmodule
